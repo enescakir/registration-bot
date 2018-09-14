5e7a6f517e7ef93ee85b797b5eea5544a505ebc7
Enis Simsar <enis.simsar@boun.edu.tr>
2018-09-14 14:28:52 +0300

    - non credit take course feature is added. - make some improvement in wording.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from registiration import Registiration
-import datetime;
+from registration import Registration
+import datetime
 import time
 
-reg = Registiration("STUDENT_ID", "STUDENT_PASSWORD")
-reg.setDebug(True)
+STUDENT_ID = "STUDENT_ID"
+STUDENT_PASSWORD = "STUDENT_PASSWORD"
+
+reg = Registration(STUDENT_ID, STUDENT_PASSWORD)
+reg.set_debug(True)
 if reg.login():
     start = time.time()
     count = 1
-    while (time.time() - start < 55):
-        print("\n---------- " + str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')))
+    while time.time() - start < 55:
+        print("\n---------- " + str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')) + "---------- " + STUDENT_ID)
         for i in range(11,15):
-            reg.changeSection("HTR", "312", str(i))
-        reg.takeCourse("CMPE", "493", "01")
+            reg.change_section("HTR", "312", str(i))
+        reg.take_course("CMPE", "493", "01")
         count += 1
         time.sleep(2)
     print("\n")
